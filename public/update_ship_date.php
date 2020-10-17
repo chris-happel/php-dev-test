ce47ee4d5f617804cb9c763fe348f6ec7e555b6a
Chris Happel <happcm01@gmail.com>
2020-10-17 03:06:55 -0400

    Added database update to update ship date and removed extra clutter.

--- public/update_ship_date.php
+++ public/update_ship_date.php
@@ -19,21 +19,21 @@
 
 			$sql_query = "select orderid, comments from sweetwater_test where comments like '%Expected Ship Date: __/__/__%'";
 			if ($query_result = $db_connection->query($sql_query)) {
-				echo "Found $query_result->num_rows rows with an expected ship date: [<br>";
+				echo "Found $query_result->num_rows rows with an expected ship date.";
 				if ($query_result->num_rows > 0) {
-					$i = 1;
 					while($row = $query_result->fetch_assoc()) {
-						$i++;
-
 						// Find the date portion of the string
 						$date_pos = strpos($row["comments"], "Expected Ship Date: ") + strlen("Expected Ship Date: ");
 						$date_str = substr($row["comments"], $date_pos, 8);
 						$date = DateTime::createFromFormat("m/d/y", $date_str);
 						$date_str = $date->format("Y-m-d");
-						echo "Row: $i Order id: " . $row["orderid"] . " | Comments: " . $row["comments"] . " | Guessed Date: $date_str" . "<br>";
+
+						$sql_update = "update sweetwater_test set shipdate_expected = '$date_str' where orderid = " . $row["orderid"];
+						if ($db_connection->query($sql_update) == FALSE) {
+							echo "Could not update orderid " . $row["orderid"] . " with found date [$date_str]. Error: $db_connection->error";
+						}
 					}
 				}
-				echo "]<br><br>";
 				$query_result->free_result();
 			}
 
